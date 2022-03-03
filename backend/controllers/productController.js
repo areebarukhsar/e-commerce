a99f21992586019b6520c6fa779d241a234ef430
Areeba Rukhsar <areebarukhsar797@gmail.com>
2022-03-03 10:53:54 -0800

    product controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,171 @@
+import asyncHandler from "express-async-handler";
+import Product from "../modals/productModal.js";
+
+// @desc:    Fetch all products
+// @route    GET /api/products
+// @access   Public
+const getProducts = asyncHandler(async (req, res) => {
+  // using .query to get what is after {? =} etc.
+  const pageSize = 4; // number of products that appears on the page
+  const page = Number(req.query.pageNumber) || 1;
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  const count = await Product.countDocuments({ ...keyword });
+
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
+});
+
+//----------------------------------
+// @desc:    Fetch a single product
+// @route    GET /api/products/:id
+// @access   Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(400);
+    throw new Error("Product not found!");
+  }
+});
+
+//----------------------------------
+// @desc:    Delete a product
+// @route    DELETE /api/products/:id
+// @access   Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(400);
+    throw new Error("Product not found!");
+  }
+});
+
+//----------------------------------
+// @desc:    Create a product
+// @route    Post /api/products
+// @access   Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Name",
+    price: 0,
+    user: req.user._id,
+    image: "//image/sample.jpg",
+    brand: "Sample Brand",
+    category: "Sample Category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample Description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+//----------------------------------
+// @desc:    Update a product
+// @route    PUT /api/products/:id
+// @access   Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    countInStock,
+    brand,
+    category,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.countInStock = countInStock;
+    product.brand = brand;
+    product.category = category;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+//----------------------------------
+// @desc:    Craete new review
+// @route    POST /api/products/:id/reviews
+// @access   Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product Already Reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: "Review Added" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+//----------------------------------
+// @desc:    Get top rated products
+// @route    GET /api/products/top
+// @access   Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+};
